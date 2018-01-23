7583c65c8563ebf7d1881bc93244b05d0212bbc6
Mehul Ved <mehul.ved@nexsales.com>
2018-01-23 13:34:51 +0530

    working parser for tmm

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ eventhtml = os.path.join(eventdir, "html")
 if not os.path.exists(eventhtml):
     os.makedirs(eventhtml)
 
-for bibno in range(1,1000,1):
-    postdata={"eventId":eventId, "eventname":eventname, "bibno":bibno}
+for bibno in range(1,10,1):
     rank = []
     participants = []
     rank_category = []
@@ -32,89 +31,110 @@ for bibno in range(1,1000,1):
 
     
     #Fetching the data
-    req = requests.post(url, data=postdata)
-    content = req.text
-    with open(os.path.join(eventhtml, str(bibno) + ".html"), 'w') as participant_html:
-        participant_html.write(content)
+    userfile = os.path.join(eventhtml, str(bibno) + ".html") 
+    if not userfile:
+        print("Requesting data for bib no: {}".format(bibno))
+        postdata={"eventId":eventId, "eventname":eventname, "bibno":bibno}
+        req = requests.post(url, data=postdata)
+        content = req.text
+        with open(userfile, 'w') as participant_html:
+            participant_html.write(content)
+    else:
+        print("Opening existing file for bib no: {}".format(bibno))
+        with open(userfile, 'r') as contentfile:
+            content = contentfile.read()
 
     # Parsing the html content
     soup = BeautifulSoup(content, "html.parser")
-    input_tags = soup.find_all('input')
-    for itag in input_tags:
-        if 'name' in itag.attrs:
-           if itag.attrs['name'] == "firstname":
-               participant_name = itag.attrs['value']
-           if itag.attrs['name'] == "chip_time":
-               nettime = itag.attrs['value']
-           if itag.attrs['name'] == "gun_time":
-               grosstime = itag.attrs['value']
-           if itag.attrs['name'] == "race_name":
-               category = itag.attrs['value']
-           if itag.attrs['name'] == "split_time":
-               splits = eval(urllib.unquote(itag.attrs['value']).decode('UTF8'))
-           if itag.attrs['name'] == "gender":
-               gender = itag.attrs['value']
-           if itag.attrs['name'] == "bracket_rank[]":
-               rank.append(itag.attrs['value'])
-           if itag.attrs['name'] == "bracket_participants[]":
-               participants.append(itag.attrs['value'])
-           if itag.attrs['name'] == "bracket_name[]":
-               rank_category.append(itag.attrs['value'])
-    
-    
-    # Write the data to a dictionary
-    
-    if participant_name == "":
-        continue
-    print("Processing bib no: {}".format(bibno))
-    result["bib"] = bibno
-    fieldnames.append("bib")
+    heading_tags = soup.find_all(['h1','h2','h3','h4','h5'])
+    if len(heading_tags) > 0:
+        participant_name = heading_tags[2].text
+        category = heading_tags[4].text
 
-    result["name"] = participant_name
-    fieldnames.append("name")
+        timing_table = soup.table.extract()
+        participant_timing = timing_table.find_all('td')
+        if len(participant_timing) > 0:
+            nettime = participant_timing[0].text
+            netpace = participant_timing[1].text
+
+        rank_table = soup.table.extract()
+        ranks = rank_table.find_all('td')
+        if len(ranks) > 0:
+            rank_overall = ranks[0].text.replace('of ','/')
+            rank_gender = ranks[1].text.replace('of ','/')
+            if len(ranks) == 2:
+                rank_ag = ""  
+            if len(ranks) == 3:
+                rank_ag = ranks[2].text.replace('of ','/')
+
+        split_list = []
+        splits_table = soup.table.extract()
+        splits_header_row = splits_table.tr.extract()
+        splits_header = splits_header_row.find_all('th')
+        if len(splits_header) > 0:
+           try:
+               while True:
+                   split_row = splits_table.tr.extract()
+                   split = split_row.find_all('td')
+                   if len(split) > 0:
+                      split_list.append(split)
+           except:
+               pass
+        
     
-    result["category"] = category
-    fieldnames.append("category")
+        # Write the data to a dictionary
+        
+        print("Processing bib no: {}\n\n".format(bibno))
+        result["bib"] = bibno
+        fieldnames.append("bib")
     
-    result["gender"] = gender
-    fieldnames.append("gender")
-    
-    result["net_time"] = nettime
-    fieldnames.append("net_time")
-    
-    result["gross_time"] = grosstime
-    fieldnames.append("gross_time") 
-    
-    for split in splits:
-        split_name = split[0].replace("+"," ")
-        split_time = split[1].replace("+"," ")
-        split_pace = split[2].replace("+"," ")
-        split_speed = split[3].replace("+"," ").replace("\\", "")
-    
-        result[split_name + " time"] = split_time
-        fieldnames.append(split_name + " time")
-    
-        result[split_name + " pace"] = split_pace
-        fieldnames.append(split_name + " pace")
-    
-        result[split_name + " speed"] = split_speed
-        fieldnames.append(split_name + " speed")
-    
-    size = 0
-    while size < len(rank_category):
-        result[rank_category[size]] = rank[size] + "/" + participants[size]
-        fieldnames.append(rank_category[size])
-        size = size + 1
-    
-   
-    #Generate dictionary of result
-    # Write to csv
-    eventfile= os.path.join(eventdir, category + ".csv")
-    file_exists = os.path.isfile(eventfile)
-    with open(eventfile, "ab") as csvfile:
-        writer = csv.DictWriter(csvfile,fieldnames=fieldnames,delimiter=",",quotechar='"',quoting=csv.QUOTE_ALL)
-        if not file_exists:
-            writer.writeheader()
-        writer.writerow(result)
+        result["name"] = participant_name
+        fieldnames.append("name")
+        
+        result["category"] = category
+        fieldnames.append("category")
+        
+        result["net_time"] = nettime
+        fieldnames.append("net_time")
+        
+        result["net_pace"] = netpace
+        fieldnames.append("net_pace") 
+        
+        result['rank_overall'] = rank_overall
+        fieldnames.append('rank_overall')
+
+        result['rank_gender'] = rank_gender
+        fieldnames.append('rank_gender')
+
+        if rank_ag != "":
+            result['rank_ag'] = rank_ag
+            fieldnames.append('rank_ag')
+
+        for split in split_list:
+           split_name = split[0].text.strip().replace('Split @ ','')
+           split_time = split[1].text.strip()
+           split_pace = split[2].text.strip()
+           split_speed = split[3].text.strip()
+        
+           result[split_name + " time"] = split_time
+           fieldnames.append(split_name + " time")
+        
+           result[split_name + " pace"] = split_pace
+           fieldnames.append(split_name + " pace")
+        
+           result[split_name + " speed"] = split_speed
+           fieldnames.append(split_name + " speed")
+        
+       
+        #Generate dictionary of result
+        # Write to csv
+        eventfile= os.path.join(eventdir, category + ".csv")
+        file_exists = os.path.isfile(eventfile)
+        with open(eventfile, "ab") as csvfile:
+            writer = csv.DictWriter(csvfile,fieldnames=fieldnames,delimiter=",",quotechar='"',quoting=csv.QUOTE_ALL)
+            if not file_exists:
+                writer.writeheader()
+            writer.writerow(result)
 
     sleep(2) # wait for 2 seconds after every request, to not overwhelm the server
+
